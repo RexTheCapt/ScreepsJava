f5b84d215083861b811215b99557bcf1bf425b95
RexTheCapt <6693554+RexTheCapt@users.noreply.github.com>
2021-03-17 15:12:35 +0100

    Updated version

--- src/main/java/com/frump/screeps/Main.java
+++ src/main/java/com/frump/screeps/Main.java
@@ -67,7 +67,7 @@ public class Main {
     private static final Array<Double> rollingAverage = new Array<>();
 
     // TODO: Get this number to automatically increment
-    private static final int version_number = 4;
+    private static final int version_number = 5;
 
     public void init(){
         if(Memory.constructionSites == null){
