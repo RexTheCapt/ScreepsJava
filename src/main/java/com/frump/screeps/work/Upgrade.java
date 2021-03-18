59338da4f6527f7997f2abc16400be3dc3984d42
RexTheCapt <6693554+RexTheCapt@users.noreply.github.com>
2021-03-18 13:35:54 +0100

    rewrite

--- src/main/java/com/frump/screeps/work/Upgrade.java
+++ src/main/java/com/frump/screeps/work/Upgrade.java
@@ -1,6 +1,6 @@
 package com.frump.screeps.work;
 
-import com.frump.screeps.Main;
+import com.frump.screeps.GameError;
 import def.screeps.Creep;
 import def.screeps.StructureController;
 
@@ -8,28 +8,54 @@ import static com.frump.screeps.CustomLogger.log;
 import static def.screeps.Globals.*;
 
 public class Upgrade {
-    public static void run(Creep creep) {
+    /**
+     * Returns true when upgrading the controller was successful.
+     * @param creep creep
+     * @return true on upgrade success.
+     * @throws Exception thrown when something goes wrong.
+     */
+    public static boolean run(Creep creep) throws Exception {
         StructureController controller = creep.room.controller;
+
+        if (controller.ticksToDowngrade < controller.progressTotal / 0.20) {
+            if (creep.store.energy == 0) {
+                creep.memory.refill = true;
+            }
+
+            if (Runner.handleRefill(creep)) {
+                creep.memory.refill = false;
+            }
+        }
 
         double res = creep.upgradeController(controller);
 
-        if (res == OK)
-            return;
-
-        if (res == ERR_NOT_ENOUGH_ENERGY)
-        {
-            if (creep.memory.role.equals(Main.role_builder)) {
-                log(creep, "cant upgrade controller, need refill");
-                creep.memory.refill = true;
-            } else
-                creep.say("energy");
-        }
-        else if (res == ERR_NOT_IN_RANGE)
-        {
-            log(creep, "moving to room controller");
+        if (res == OK) {
+            log(creep, "controller is upgraded");
+            return true;
+        } else if (res == ERR_NOT_OWNER) {
+            log(creep, "not owner");
+            // TODO: to something if not mine?
+            return false;
+        } else if (res == ERR_BUSY) {
+            log(creep, "still spawning");
+            return false;
+        } else if (res == ERR_NOT_ENOUGH_RESOURCES) {
+            log(creep, "need refill");
+            creep.memory.refill = true;
+            Runner.handleRefill(creep);
+            return false;
+        } else if (res == ERR_INVALID_TARGET) {
+            log(creep, "invalid target");
+            throw GameError.newInvalidTarget(creep);
+        } else if (res == ERR_NOT_IN_RANGE) {
+            log(creep, "moving to controller");
             creep.moveTo(controller.pos);
+            return false;
+        } else if (res == ERR_NO_BODYPART) {
+            log(creep, "no body part");
+            throw GameError.newNoBodyPart(creep);
+        } else {
+            throw GameError.newUnhandledCode(creep, res);
         }
-        else
-            creep.say("C: " + res);
     }
 }
