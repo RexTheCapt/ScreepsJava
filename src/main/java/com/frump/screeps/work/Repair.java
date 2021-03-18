31e16fc4655b8df3cd4ea2a8ccf2e6c38e157fb4
RexTheCapt <6693554+RexTheCapt@users.noreply.github.com>
2021-03-18 13:33:50 +0100

    updated

--- src/main/java/com/frump/screeps/work/Repair.java
+++ src/main/java/com/frump/screeps/work/Repair.java
@@ -22,40 +22,54 @@ public class Repair {
     public static boolean run(Creep creep) throws Exception {
         if (creep.store.energy == 0) {
             creep.memory.refill = true;
-            creep.memory.destinationId = null;
-        } else if (creep.store.getFreeCapacity(RESOURCE_ENERGY) == 0 && creep.memory.refill) {
-            log(creep, "back to work");
-            creep.memory.refill = false;
-            creep.memory.destinationId = null;
         }
 
         if (creep.memory.refill) {
-            refill(creep);
+            if (Runner.handleRefill(creep))
+                creep.memory.refill = false;
+            return false;
         } else {
-            repairStructure(creep);
+            return repairStructure(creep);
         }
     }
 
-    private static void repairStructure(Creep creep) {
+    private static boolean repairStructure(Creep creep) throws Exception {
         Structure structure = getStructure(creep);
 
         if (structure == null || structure.hits == structure.hitsMax) {
             log(creep, "structure invalid or repaired");
             creep.memory.destinationId = null;
-            return;
+            return false;
         }
 
         double res = creep.repair(structure);
 
         if (res == OK) {
             log(creep, "repairing, hits left: " + (structure.hitsMax - structure.hits));
+            return true;
+        } else if (res == ERR_NOT_OWNER) {
+            log(creep, "not my structure");
+            // TODO: Do something if not mine.
+            return false;
+        } else if (res == ERR_BUSY) {
+            log(creep, "still spawning");
+            return false;
+        } else if (res == ERR_NOT_ENOUGH_RESOURCES) {
+            log(creep, "need refill");
+            creep.memory.refill = true;
+            return false;
+        } else if (res == ERR_INVALID_TARGET) {
+            log(creep, "invalid target");
+            throw GameError.newInvalidTarget(creep);
+        } else if (res == ERR_NOT_IN_RANGE) {
+            log(creep, "moving to target");
+            creep.moveTo(structure.pos);
+            return false;
+        } else if (res == ERR_NO_BODYPART) {
+            log(creep, "missing body part");
+            throw GameError.newNoBodyPart(creep);
         } else {
-            if (res == ERR_NOT_IN_RANGE) {
-                log(creep, "moving to structure");
-                creep.moveTo(structure.pos);
-            } else {
-                log(creep, res, "Repair.repairStructure");
-            }
+            throw GameError.newUnhandledCode(creep, res);
         }
     }
 
@@ -88,65 +102,6 @@ public class Repair {
         }
 
         return structure;
-    }
-
-    private static void refill(Creep creep) {
-        StructureContainer container = getEnergyContainer(creep);
-
-        if (container == null) {
-            log(creep, "resetting refill destination");
-            creep.memory.destinationId = null;
-            return;
-        }
-
-        double res = creep.withdraw(container, RESOURCE_ENERGY);
-
-        if (res == OK) {
-            log(creep, "refilled energy, " + creep.store.getFreeCapacity(RESOURCE_ENERGY) + " left");
-
-            if (creep.store.getFreeCapacity(RESOURCE_ENERGY) == 0) {
-                log(creep, "refill complete");
-                creep.memory.refill = false;
-            }
-        } else {
-            if (res == ERR_NOT_IN_RANGE) {
-                log(creep, "moving to container");
-                creep.moveTo(container.pos);
-            } else if (res == ERR_NOT_ENOUGH_ENERGY) {
-                log(creep, "not enough energy");
-                creep.memory.destinationId = null;
-            } else {
-                log(creep, res, "Repair.refill");
-            }
-        }
-    }
-
-    private static void log(Creep creep, double result, String text) {
-        CustomLogger.log(creep, result, text);
-    }
-
-    private static StructureContainer getEnergyContainer(Creep creep) {
-        StructureContainer container;
-
-        if (creep.memory.destinationId == null || creep.memory.destinationId.isEmpty()) {
-            log(creep, "finding closest container");
-            container = creep.pos.findClosestByPath(
-                    FIND_STRUCTURES,
-                    Helper.findFilter((Structure s) ->
-                            s.structureType.equals(STRUCTURE_CONTAINER) &&
-                                    ((StructureContainer) s).store.energy >= creep.store.getFreeCapacity(RESOURCE_ENERGY)
-                    )
-            );
-
-            if (container != null) {
-                creep.memory.destinationId = container.id;
-            }
-        } else {
-            log(creep, "finding container by id");
-            container = Game.getObjectById(creep.memory.destinationId);
-        }
-
-        return container;
     }
 
     private static void log(Creep creep, String text) {
